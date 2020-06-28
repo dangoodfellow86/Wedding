78f31a4661fb1d408ab2fd6f56aa6d451273a8a5
Dan <dan@dans-mbp.home>
2020-06-28 17:50:30 +0100

    add countdown script

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+var count = new Date("aug 19,2021 15:30:00").getTime();
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var d = count - now;
+
+    var days = Math.floor((d/(1000*60*60*24)));
+    var hours = Math.floor((d%(1000*60*60*24))/(1000*60*60));
+    var minutes = Math.floor((d%(1000*60*60))/(1000*60));
+    var seconds = Math.floor((d%(1000*60))/1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours; 
+    document.getElementById("mins").innerHTML = minutes; 
+    document.getElementById("seconds").innerHTML = seconds;
+},1000);
